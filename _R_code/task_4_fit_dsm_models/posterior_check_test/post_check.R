947879e2b83618779e41cb7f73164682db6b95e2
Devin Johnson <devin.johnson@noaa.gov>
2026-03-13 12:10:41 -0600

    Update post_check.R

--- _R_code/task_4_fit_dsm_models/posterior_check_test/post_check.R
+++ _R_code/task_4_fit_dsm_models/posterior_check_test/post_check.R
@@ -21,10 +21,9 @@ supp_files <- list.files(path = here("_R_code","task_8_propagating_variance","su
 for(i in seq_along(supp_files)) source(supp_files[i])
 
 draw_tw <- function(mu, p, phi, ...){
-  nc <- ifelse(is.null(ncol(mu)), 1, ncol(mu))
-  nr <- ifelse(is.null(nrow(mu)), length(mu), nrow(mu))
+  nr <- length(mu)
   o <- sapply(1:nr,
-              function(i, mu, p, phi){mgcv::rTweedie(mu[i,],p,phi)},
+              function(i, mu, p, phi){mgcv::rTweedie(mu[i],p,phi)},
               mu=mu, p=p, phi=phi
   )
   return(as.vector(o))
@@ -48,19 +47,29 @@ b_gs <- rbind(gs_fit$coefficients, beta_gs_samp$bs[idx,])
 # Simulate data
 p <- er_fit$family$getTheta(TRUE)
 phi <- summary(er_fit)$dispersion
+sig <- sqrt(gs_fit$sig2)
 Ler <- predict(er_fit, newdata=fkw_dsm_data, type="lpmatrix")
 Lgs <- predict(gs_fit, newdata=fkw_dsm_data, type="lpmatrix")
 
 nz <- NULL
 dens <- NULL
+n <- nrow(fkw_dsm_data)
+mean_eff <- mean(fkw_dsm_data$effort)
 for(i in 1:nrow(b_er)){
-  pred_er <- as.vector(exp(Ler %*% b_er[i,]+ log(fkw_dsm_data$effort)))
-  pred_gs <- as.vector(exp(Lgs%*%b_gs[i,] + 0.5*gs_fit$sig2))
-  modeldens <- pred_er * pred_gs * gs_bias_corr
-  preddens <- sum(modeldens)/sum(fkw_dsm_data$effort)
-  dens <- c(dens,preddens*A)
+  mu <- as.vector(exp(Ler %*% b_er[i,]+ log(fkw_dsm_data$effort)))
   tt <- draw_tw(mu, p, phi)
+  ss <- exp(rnorm(n, mean=Lgs %*% b_gs[i,], sd=sig))
+  y <- ss*tt
+  dens <- c(dens, 100*mean(y)/mean_eff)
   nz <- c(nz, sum(tt>0))
+}
+  
+  # pred_gs <- as.vector(exp(Lgs%*%b_gs[i,] + 0.5*gs_fit$sig2))
+  # modeldens <- pred_er * pred_gs * gs_bias_corr
+  # preddens <- sum(modeldens)/sum(fkw_dsm_data$effort)
+  # dens <- c(dens,preddens*A)
+
+
 }
 
 
